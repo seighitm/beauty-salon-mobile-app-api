9136dc7124fd92e5b2e1f044ba3805f6edecd05c
seighitm <seighitm@mail.ru>
2021-11-01 13:46:06 +0200

    Created database connection

--- backend/server.ts
+++ backend/server.ts
@@ -1,11 +1,27 @@
+const mongoose = require("mongoose");
 const express = require("express");
-require("dotenv").config()
+const bodyParser = require("body-parser");
+const cors = require("cors");
+require("dotenv").config();
 
 const app = express();
 
-const PORT: number | string = process.env.PORT || 8080;
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+mongoose.connect(
+    process.env.DB_CONNECT, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    })
+    .then(() => console.log('MongoDB Connected'))
+    .catch((err: any) => console.log(err));
+
+const PORT: number | string = process.env.PORT || 7000;
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}.`);
 });
+
 
